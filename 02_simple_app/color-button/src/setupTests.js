76604630f356c671280ba82e2576b25a8d04c2e7
Jacob Hallman <jacobhallman94@gmail.com>
2021-02-24 17:31:02 +0100

    Added setupTests comment

--- 02_simple_app/color-button/src/setupTests.js
+++ 02_simple_app/color-button/src/setupTests.js
@@ -1,3 +1,5 @@
+// This file is automatically picked up by React for global test setup things, see docs at: https://create-react-app.dev/docs/running-tests/#srcsetuptestsjs
+
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
